a315799dea31a885850ef1b6e03c9116856ac7c4
krishnanpandya007 <krishnanpandya06@gmail.com>
2023-01-02 01:11:39 +0530

    Direct App open on website load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { useEffect, useRef } from "react"
+
 export default function App() {
+  const appRef = useRef(null);
+
+  useEffect(() => {
+    if(appRef.current != null){
+      appRef.current.click();
+    }
+  }, [appRef.current]);
 
   return (
     <a href="reachoutapp://reachout.org.in/welcome">Open the APK</a>
