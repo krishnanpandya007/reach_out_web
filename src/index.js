1d30d9b084e9cf37458f52842599c83e1022e1ae
krishnanpandya007 <krishnanpandya06@gmail.com>
2023-06-03 11:16:01 +0530

    Added Updated App/Index files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    {/* <React.StrictMode> */}
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </React.StrictMode> */}
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
