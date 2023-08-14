d1abd47678d35f32050c0d4d3796059ae203bbfe
krishnanpandya007 <krishnanpandya06@gmail.com>
2023-08-14 23:29:11 +0530

    updated backend url with secured one

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const socials = [
 
 // export const FRONTEND_ROOT_URL = "https://reach-out-web.vercel.app";
 export const FRONTEND_ROOT_URL = "https://reachout.org.in";
-export const BACKEND_ROOT_URL = 'http://165.22.208.173'
+export const BACKEND_ROOT_URL = 'https://backend-61489.reachout.org.in'
 export const base_json_header = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
